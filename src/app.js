bf568b6f7b080fd9710ffdf2331de85d3350b915
Gabriel Nicutari <gabriel_nicutari@yahoo.ro>
2022-12-07 13:09:11 +0100

    More cleanup, fix public __dirname

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const http = require('http');
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
 const app = express();
 
 let server = http.createServer(app);
@@ -7,29 +8,71 @@ const fs = require("fs");
 
 app.use(express.static("public"));
 
-const header = fs.readFileSync(__dirname + "/public/header/header.html", "utf-8");
-const sideBar = fs.readFileSync(__dirname + "/public/side-bar/side-bar.html", "utf-8");
-const mainPage = fs.readFileSync(__dirname + "/public/main-page/main-page.html", "utf-8");
-const footer = fs.readFileSync(__dirname + "/public/footer/footer.html", "utf-8");
-const create = fs.readFileSync(__dirname + "/public/create-modal/create-modal.html", "utf-8");
-const deleteAccount = fs.readFileSync(__dirname + "/public/delete-modal/delete-modal.html", "utf-8");
-const edit = fs.readFileSync(__dirname + "/public/edit-modal/edit-modal.html", "utf-8");
-const notification = fs.readFileSync(__dirname + "/public/notification-modal/notification-modal.html", "utf-8");
-const login = fs.readFileSync(__dirname + "/public/login/login.html", "utf-8");
-const liveChat = fs.readFileSync(__dirname + "/public/live-chat/live-chat.html", "utf-8");
+const path = require("path");
+
+const header = fs.readFileSync(
+  path.join(__dirname, "../public/header/header.html"),
+  "utf-8"
+);
+const sideBar = fs.readFileSync(
+  path.join(__dirname, "../public/side-bar/side-bar.html"),
+  "utf-8"
+);
+const mainPage = fs.readFileSync(
+  path.join(__dirname, "../public/main-page/main-page.html"),
+  "utf-8"
+);
+const footer = fs.readFileSync(
+  path.join(__dirname, "../public/footer/footer.html"),
+  "utf-8"
+);
+const create = fs.readFileSync(
+  path.join(__dirname, "../public/create-modal/create-modal.html"),
+  "utf-8"
+);
+const deleteAccount = fs.readFileSync(
+  path.join(__dirname, "../public/delete-modal/delete-modal.html"),
+  "utf-8"
+);
+const edit = fs.readFileSync(
+  path.join(__dirname, "../public/edit-modal/edit-modal.html"),
+  "utf-8"
+);
+const notification = fs.readFileSync(
+  path.join(__dirname, "../public/notification-modal/notification-modal.html"),
+  "utf-8"
+);
+const login = fs.readFileSync(
+  path.join(__dirname, "../public/login/login.html"),
+  "utf-8"
+);
+const liveChat = fs.readFileSync(
+  path.join(__dirname, "../public/live-chat/live-chat.html"),
+  "utf-8"
+);
 
 // UI Calls
-app.get('/', checkCookieMiddleware, (req, res) => {
-    res.send(header + sideBar + mainPage + create + deleteAccount + edit + notification +  liveChat  + footer);
-}); 
+app.get("/", (req, res) => {
+  res.send(
+    header +
+      sideBar +
+      mainPage +
+      create +
+      deleteAccount +
+      edit +
+      notification +
+      liveChat +
+      footer
+  );
+});
 
-app.get('/login', (req, res) => {
-    res.send(header + login + footer);
+app.get("/login", (req, res) => {
+  res.send(header + login + footer);
 });
 
 server.listen(process.env.PORT, (error) => {
-    if (error) {
-        console.log(error);
-    }
-    console.log("App listening on localhost :", process.env.PORT);
+  if (error) {
+    console.log(error);
+  }
+  console.log("App listening on localhost :", process.env.PORT);
 });
