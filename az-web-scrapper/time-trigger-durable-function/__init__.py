3d8aae144032ee6874ed8d18f96b0d0d007486c0
Itai Gramse <itai0004@stud.kea.dk>
2022-12-23 15:36:58 +0100

    chain azure timer trigger function with azure orchetrator

--- az-web-scrapper/time-trigger-durable-function/__init__.py
+++ az-web-scrapper/time-trigger-durable-function/__init__.py
@@ -2,9 +2,10 @@ import datetime
 import logging
 
 import azure.functions as func
+import azure.durable_functions as df
 
 
-def main(mytimer: func.TimerRequest) -> None:
+async def main(mytimer: func.TimerRequest, starter: str) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
 
@@ -12,3 +13,8 @@ def main(mytimer: func.TimerRequest) -> None:
         logging.info('The timer is past due!')
 
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
+
+    client = df.DurableOrchestrationClient(starter)
+    print("client has been created")
+    instance_id = await client.start_new("durable-function-web-scapper")
+
