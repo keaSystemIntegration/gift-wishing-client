e1fe29b51ef2fba3b78bf9dca5775ccc0b54c1e8
Gabriel <gabriel_nicutari@yahoo.ro>
2023-01-03 17:34:10 +0100

    Friends rework

--- public/friends/scripts.js
+++ public/friends/scripts.js
@@ -1,64 +1,64 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+(function socket() {
+  console.log("hi");       
+  let socket = null;
+  const accessToken = window.localStorage.token;
+  const url = "https://api.gifts.hotdeals.dev";
 
-let socket = null;
-const accessToken = window.localStorage.token;
-const url = "https://api.gifts.hotdeals.dev";
-
-(async function() {
+  (async function connection() {
     socket = io(url, {
-    transports: ["polling"],
-    auth: {
-      token: accessToken
-    },
-  });
+      transports: ["polling"],
+      auth: {
+        token: accessToken,
+      },
+    });
 
-  const response = axios.get("/wishes", {
-    headers: {
-      authorization:
-        `Bearer ${accessToken}`,
-    },
-  });
-  const result = await response;
-  // console.log(result.data);
-  window.localStorage.setItem("wishes", JSON.stringify(result.data));
-  // console.log(window.localStorage);
-})();
+    const response = axios.get("/wishes", {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const result = await response;
+    // console.log(result.data);
+    window.localStorage.setItem("wishes", JSON.stringify(result.data));
+    // console.log(window.localStorage);
+  })();
 
-socket.emit("status");
+  socket.emit("status");
 
-socket.on("statusUpdate", (data) => {
-  const friends = data || [];
-  // console.log(friends);
-  if (friends.length > 0) {
-    $('#friends__list').html(function() {
-      return friends.map((friend, index) => {
-        let status = null;
-        switch(friend.status) {
-          case 'online':
-            status = 'online';
-            break;
-          case 'offline':
-            status = 'offline';
-          default:
-            status = 'not-registered';
-        }
+  socket.on("statusUpdate", (data) => {
+    const friends = data || [];
+    // console.log(friends);
+    if (friends.length > 0) {
+      $("#friends__list").html(function () {
+        return friends.map((friend, index) => {
+          let status = null;
+          switch (friend.status) {
+            case "online":
+              status = "online";
+              break;
+            case "offline":
+              status = "offline";
+            default:
+              status = "not-registered";
+          }
 
-        
-        return `
+          return `
         <div class="friend__item" id="${friend.email}">
           <span class="friend__item_status friend__item_status-${status}" title="${status}"></span>
           <span class="friend__item_index">${++index}.</span>
           <span class="friend__item_email">${friend.email}</span>
         </div>
-        `});
-    });
-  } else {
-    $('#friends--list').append(`
+        `;
+        });
+      });
+    } else {
+      $("#friends--list").append(`
       <div class="friends--list-empty">No friends yet</div>
     `);
-  }
-});
+    }
+  });
 
-socket.on("refresh", () => {
-  socket.emit("status");
-});
+  socket.on("refresh", () => {
+    socket.emit("status");
+  });
+})();
